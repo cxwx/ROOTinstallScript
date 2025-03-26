f77193d16bcee753a040f7ae59acc9e96427843d
chenxu <6235641+cxwx@users.noreply.github.com>
2025-03-26 10:27:20 +0800

    remove unnecessary

--- sh4_install_root_cmake.sh
+++ sh4_install_root_cmake.sh
@@ -26,48 +26,20 @@ cmake -DCMAKE_INSTALL_PREFIX="${HOME}/software/ROOT/install/" ../root ${CMAKE_PA
   -DCMAKE_CXX_STANDARD=20 \
   -Dbuiltin_clang=ON \
   -Dbuiltin_cling=ON \
-  -Dmathmore=ON \
-  -Darrow=OFF \
-  -Dasimage=ON \
-  -Dbuiltin_vdt=ON \
-  -Dvdt=ON \
-  -Dxrootd=ON \
-  -Dbuiltin_xrootd=ON \
-  -Dvc=ON \
-  -Dbuiltin_vc=OFF \
-  -Dveccore=ON \
-  -Dbuiltin_veccore=OFF \
+  -Dfftw3=ON \
   -Dxml=ON \
   -Dunfold=ON \
-  -Dminimal=ON \
-  -DCXX_STANDARD_STRING=17 \
-  -Dclad=ON \
-  -DCMAKE_CXX_EXTENSIONS=ON \
-  -Dfail-on-missing=ON \
-  -Dfcgi=ON \
-  -Dtmva-sofie=ON \
-  -Dvecgeom=ON \
-  -Ddev=ON \
-  -Dpyroot=ON \
-  -Dtpython=ON \
-  -Ddataframe=ON \
-  -Ddavix=ON \
-  -Dfftw3=ON \
-  -Dfitsio=ON \
-  -Dgdml=ON \
-  -Dgeom=ON \
   -Dgviz=ON \
-  -Dhttp=ON \
-  -Dopengl=ON \
-  -Dproof=ON \
-  -Droofit=ON \
-  -Droot7=ON \
-  -Drpath=ON \
-  -Dspetrum=ON \
-  -Dsqlite=ON \
-  -Dtmva=ON \
-  -Dtmva-cpu=ON \
-  -Dwebgui=ON \
+  -Dveccore=ON \
+  -DGviz=ON \
+  -Dfcgi=ON \
+  -Darrow=ON \
+  -Ddavix=ON \
+  -DVc=ON \
+  -Dminimal=ON \
+  -Dmathmore=ON \
+
+  # -Dfail-on-missing=ON \
 
 # -Dmacos_native=OFF  # pcre bugs
 # removed # -Dbuiltin_glew=ON
@@ -84,7 +56,7 @@ cmake -DCMAKE_INSTALL_PREFIX="${HOME}/software/ROOT/install/" ../root ${CMAKE_PA
 
 # -Dminuit2_omp=ON \
 # CMake Warning at cmake/modules/RootBuildOptions.cmake:409 (message):
-  # The option 'minuit2_omp' can only be used to minimise thread-safe functions
-  # in Minuit2.  It cannot be used for Histogram/Graph fitting and for RooFit.
-  # If you want to use Minuit2 with OpenMP or MPI support, it is better to
-  # build Minuit2 as a standalone library.
+# The option 'minuit2_omp' can only be used to minimise thread-safe functions
+# in Minuit2.  It cannot be used for Histogram/Graph fitting and for RooFit.
+# If you want to use Minuit2 with OpenMP or MPI support, it is better to
+# build Minuit2 as a standalone library.
